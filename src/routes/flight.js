0c016c969001234c345621d365d3be0a130e821f
axel-ac <abdullahceylan.axel@gmail.com>
2023-10-20 20:07:09 +0200

    routes

--- src/routes/flight.js
+++ src/routes/flight.js
@@ -6,9 +6,23 @@ const router = require("express").Router();
 /* ------------------------------------------------------- */
 // routes/Flight:
 
+const permissions = require("../middlewares/permissions");
+
 const Flight = require("../controllers/Flight");
 
 // URL: /Flights
+
+// router.route('/')
+//     .get(permissions.isStaffOrAdmin, flight.list)
+//     .post(permissions.isStaffOrAdmin, flight.create)
+
+// router.route('/:id')
+//     .get(permissions.isStaffOrAdmin, flight.read)
+//     .put(permissions.isStaffOrAdmin, flight.update)
+//     .patch(permissions.isStaffOrAdmin, flight.update)
+//     .delete(permissions.isAdmin, flight.delete)
+
+router.use(permissions.isStaffOrAdmin)
 
 router.route("/").get(Flight.list).post(Flight.create);
 
