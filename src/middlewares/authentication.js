c262d2c578950ee5edd620261e46cd92a15e2905
axel-ac <abdullahceylan.axel@gmail.com>
2023-10-20 20:12:18 +0200

    middlewares

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -12,6 +12,14 @@ module.exports = (req, res, next) => {
     const auth = req.headers?.authorization || null
     const accessToken = auth ? auth.split(' ')[1] : null
 
-    jwt.verify(accessToken, process.env.ACCESS_KEY, (err, userData) => req.user = userData)
-    next()
+      jwt.verify(
+        accessToken,
+        process.env.ACCESS_KEY,
+        (err, userData) => (req.user = userData)
+      );
+
+      // Add createdID for all req.body:
+      req.body.createdId = req.user?._id;
+
+      next();
 }
--- src/middlewares/permissions.js
+++ src/middlewares/permissions.js
@@ -1,44 +1,34 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
 // Middleware: permissions
 
 module.exports = {
+  isLogin: (req, res, next) => {
+    if (req.user) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission: You must login.");
+    }
+  },
 
-    isLogin: (req, res, next) => {
+  isStaffOrAdmin: (req, res, next) => {
+    if (req.user && (req.user.isStaff || req.user.isAdmin)) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission: You must login and to be Staff.");
+    }
+  },
 
-        return next()
-        
-        if (req.user) {
-            next()
-        } else {
-            res.errorStatusCode = 403
-            throw new Error('NoPermission: You must login.')
-        }
-    },
-
-    isAdmin: (req, res, next) => {
-
-        return next()
-        
-        if (req.user && req.user.isAdmin) {
-            next()
-        } else {
-            res.errorStatusCode = 403
-            throw new Error('NoPermission: You must login and to be Admin.')
-        }
-    },
-
-    isStaff: (req, res, next) => {
-
-        return next()
-        
-        if (req.user && (req.user.isAdmin || req.user.isStaff)) {
-            next()
-        } else {
-            res.errorStatusCode = 403
-            throw new Error('NoPermission: You must login and to be Staff.')
-        }
-    },
-}
+  isAdmin: (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission: You must login and to be Admin.");
+    }
+  },
+};
